5e5479ea38754cb0cb536c49bfb1b25a6c73477f
kathesm903 <kathesm903@gmail.com>
2018-05-28 23:36:19 -0400

    Vistas realizadas, falta vista de consulta especializacion(terminar)

--- serina/polls/urls.py
+++ serina/polls/urls.py
@@ -7,6 +7,9 @@ from . import views
 urlpatterns = [
     path('index/', views.index, name ='index'),
     path('formulario/', views.form_categoria, name = 'form_categoria'),
-    path('formulario/prueba', views.prueba, name = 'prueba'),
+    path('formulario/verCateg', views.ver_categ, name = 'ver_categ'),
     path('formulario/addcategoria', views.add_categoria, name = 'add_categoria'),
+    path('formulario/addcita', views.add_cita, name = 'add_cita'),
+    path('formulario/consultarcateg', views.consultar_categ, name = 'cosultar_categ'),
+    path('formulario/addIndicaciones', views.add_indicaciones, name = 'add_indicaciones'),
 ]
--- serina/polls/views.py
+++ serina/polls/views.py
@@ -3,21 +3,48 @@ from django.views.generic import ListView, CreateView
 from django.http import HttpResponse, HttpResponseRedirect
 from .forms import FormCategoria
 from .models import Categoria
+
 # Create your views here.
 def index(request):
     return render(request, 'serina_views/index.html')
 
-def prueba(request):
-    return render(request, 'serina_views/prueba.html')
+def ver_categ(request):
+    return render(request, 'serina_views/ver_categ.html')
 
 def form_categoria(request):
     return render(request, 'serina_views/formTratamiento.html')
 
 def add_categoria(request):
-    form = FormCategoria(request.POST or None)
-    if form.is_valid():
-        form_data = form.cleaned_data
-        categoria = form_data.get("nombre")
-        obj = Categoria.objects.create(nombre = categoria)
-    context = {'form': form}
-    return render(request, "serina_views/prueba.html", context)
+
+    categoria = request.POST['categoria']
+    context = {'categoria': categoria}
+    db_o = Categoria(nombre=categoria)
+    db_o.save()
+    return render(request, "serina_views/ver_categ.html", context)
+
+def add_cita(request):
+
+    fecha = request.POST['fecha']
+    nota = request.POST['nota']
+    context = {'fecha': fecha, 'nota': nota}
+    db_fecha = Cita(fecha = fecha)
+    db_fecha.save()
+    db_nota = Cita(nota = nota)
+    db_nota.save()
+    return render(request, "serina_views/ver_categ.html", contex)
+
+
+def add_indicaciones(request):
+
+    #medicina = request.POST['medicina']
+    #context = {'medicina': medicina}
+
+    #db_medicina = Medicina(nombre = medicina)
+
+    return render(request, "serina_views/form_indicaciones.html")
+
+
+
+def consultar_categ(resquest):
+    categoria = Categoria.objects.all()
+    return render(request, "serina_views/ver_categ.html", {'categoria': categoria})
